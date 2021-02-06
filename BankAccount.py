f91d7cd87902c3717ca0339ebac6cabbbfda2718
Kristina <cyberkitty628@gmail.com>
2021-02-05 16:31:34 -0800

    Created deposit method

--- BankAccount.py
+++ BankAccount.py
@@ -1,11 +1,18 @@
+import random 
+
 class BankAccount:
 
-    def __init__(self, full_name, account_number, routing_number, balance):
+    def __init__(self, full_name):
         self.full_name = full_name
-        self.account_number = account_number
-        self.routing_number = routing_number
-        self.balance = balance
+        self.account_number = random.randint(00000000, 99999999)
+        self.routing_number = 987654321
+        self.balance = 0
 
-account_1 = BankAccount('Kristina Kamphuis', 123456789, 987654321, 1500)
+    def deposit(self, amount):
+        self.balance += amount
 
-print(account_1.routing_number)
+account_1 = BankAccount('Kristina Kamphuis')
+
+account_1.deposit(1000)
+
+print(account_1.balance)
