80486e7b555e4628cba6135a627d9d68aecbee72
Kristina <cyberkitty628@gmail.com>
2021-02-08 02:48:37 -0800

    Added more methods

--- BankAccount.py
+++ BankAccount.py
@@ -10,9 +10,22 @@ class BankAccount:
 
     def deposit(self, amount):
         self.balance += amount
+        print(f'Amount Deposited: ${float(amount)}')
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        print(f'Amount Withdrawn: ${float(amount)}')
+
+    def get_balance(self):
+        print(f'Your account balance is {self.balance}.')
+        return self.balance
 
 account_1 = BankAccount('Kristina Kamphuis')
 
-account_1.deposit(1000)
+account_1.deposit(800)
 
-print(account_1.balance)
+account_1.withdraw(600)
+
+# print(account_1.balance)
+
+account_1.get_balance()
